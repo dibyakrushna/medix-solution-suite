9f2d26a78cd2a170ef535fdcf4ec875ebabe8c9c
dibya_geeks <dibya.das@magnigeeks.com>
2024-10-19 03:51:56 +0530

    Plugin description

---
+++ medix-solution-suite.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Plugin Name: Medix Solution Suite
+ * Description: Medix Solution Suite is a powerful, all-encompassing WordPress plugin designed to streamline and simplify healthcare management. Built with the modern medical facility in mind, this comprehensive suite offers everything healthcare providers need to manage patients, staff, appointments, billing, and more—directly from your WordPress dashboard.
+ * 
+ * **/
